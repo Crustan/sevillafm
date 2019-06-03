8820aa83b2ecd07d97b36a81721020a24c304fc9
Christian Söderberg <christian.v.soderberg@gmail.com>
2019-06-03 15:41:55 +0200

    Add favicon

--- src/components/seo.js
+++ src/components/seo.js
@@ -10,6 +10,9 @@ import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
 
+import favicon16 from "../images/favicon16.png"
+import favicon32 from "../images/favicon32.png"
+
 function SEO({ description, lang, meta, keywords, title }) {
   const { site } = useStaticQuery(
     graphql`
@@ -34,6 +37,20 @@ function SEO({ description, lang, meta, keywords, title }) {
       }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
+      link={[
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: `${favicon16}`,
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: `${favicon32}`,
+        },
+      ]}
       meta={[
         {
           name: `description`,
